23a4a49a3c159792eb2ba440387b08064ee94ba3
Andrei Toma <3350272+homepods@users.noreply.github.com>
2025-03-23 16:07:48 +0200

    Update setup.sh

--- Scripts/setup.sh
+++ Scripts/setup.sh
@@ -1,54 +1,77 @@
 #!/bin/bash
-# -- version 1.0 hookprobe.com 
+# -- version 1.0 hookprobe.com --- Ubuntu 24.10
 set -e
 
-# --- Update system and install Podman & OpenVirtualSwitch ---
-echo "Updating package lists and installing Podman..."
+# --- Update system and install appman & OpenVirtualSwitch ---
+echo "Updating package lists and installing appman..."
 sudo apt-get update
-sudo apt-get install -y podman
+sudo apt-get install -y appman
 
-sudo apt install -y openvswitch-switch
+sudo apt-get install -y openvswitch-switch
 
-# --- Podman images for the web app containers ---
-podman image pull postgres:alpine
-podman image pull python:3.12-slim
-podman image pull nginx:alpine
+# --- appman images for the web app containers ---
+appman image pull postgres:alpine
+appman image pull python:3.12-slim
+appman image pull nginx:alpine
 
-# --- Create an APP NAME Bridge & POD for your application --- #PSQL_KEY = 4bit , NETWORK_NAME = should be none to use nsenter. custom build to use NAT and PAT for security increase
+# --- Create an APP NAME Bridge & app for your application --- #PSQL_KEY = 4bit , NETWORK_NAME = should be none to use nsenter. custom build to use NAT and PAT for security increase
 APP_NAME="your_app"
-APP_PSK="password_for_vxlan"
 NETWORK_NAME="none" 
-PSQL_IP=10.20.30.255
+
+# --- alocate IP variables for OpenVirtual Switch & IP vor vxlan
+VXLAN_IP=192.168.0.1
 PSQL_KEY=1111
-DB_APP=your_DB_volume    
+APP_PSK="password_for_vxlan"
+PSQL_OVS=psql-ovs
+PSQL_NETNS=psql-netns
+DJANGO_OVS=django-ovs
+DJANGO_NETNS=django-netns
+NGINX_OVS=nginx-ovs
+NGINX_NETNS=nginx-netns
+CFL_OVS=cfl-ovs
+CFL_NETNS=cfl-netns
+
+# --- alocate IP variables for all Containers
+CT_DEF_GW=10.20.30.1
+PSQL_IP=10.20.30.255
+DJANGO_IP=10.20.30.254
+NGINX_IP=10.20.30.253
+CFL_IP=10.20.30.252
+
+# --- Container Names variables
+
+DB_CONTAINER_NAME="your_app_psql"
+DJANGO_CT_NAME="django"
+NGINX_CT_NAME="nginx"
+CLOUDFLARED_CT_NAME="cloudflared"
 
 # --- Database Variabales for PSQL Container
-DB_CONTAINER_NAME="your_app_psql"
 DB_USER="your_user"
 DB_PASSWORD="your_secret_password"
 DB_NAME="database"
+DB_APP=your_DB_volume
 
 #Flush all created components
 echo "Flushing existing setup from system"
 
-podman container stop ${DB_CONTAINER_NAME}
-podman container rm ${DB_CONTAINER_NAME}
+appman container stop ${DB_CONTAINER_NAME}
+appman container rm ${DB_CONTAINER_NAME}
 
-podman container stop django
-podman container rm djngo
+appman container stop ${DJANGO_CT_NAME}
+appman container rm ${DJANGO_CT_NAME}
 
-podman container stop nginx
-podman container rm nginx
+appman container stop ${NGINX_CT_NAME}
+appman container rm ${NGINX_CT_NAME}
 
-podman container stop cloudflared
-podman container rm cloudflared
+appman container stop ${CLOUDFLARED_CT_NAME}
+appman container rm ${CLOUDFLARED_CT_NAME}
 
 sudo ovs-vsctl del-br ${APP_NAME}
 sudo ip netns delete ${APP_NAME}
-podman volume rm ${DB_APP} 
-podman pod rm ${APP_NAME}
-sudo ip link delete dev pod-gatewayhost
-sudo ip link delete dev pod-gatewayovs
+appman volume rm ${DB_APP} 
+appman app rm ${APP_NAME}
+sudo ip link delete dev app-gatewayhost
+sudo ip link delete dev app-gatewayovs
 rm -r web_app
 rm -r nginx_conf
 rm -r cloudflared
@@ -58,13 +81,13 @@ echo "Flush Complete...."
 echo "Setup Application. Please wait ...."
 
 # --- Begin by starting fresh a new network ap from here ---
-podman pod create ${APP_NAME}
-podman volume create ${DB_APP}
+appman app create ${APP_NAME}
+appman volume create ${DB_APP}
 
-# --- Create a Podman network for inter-container communication with ROOT containers ^change --net to ${NETWORK_NAME} ---
+# --- Create a appman network for inter-container communication with ROOT containers ^change --net to ${NETWORK_NAME} ---
 #NETWORK_NAME="mynetwork"
-#echo "Creating Podman network '${NETWORK_NAME}'..."
-#sudo podman network create ${NETWORK_NAME} || echo "Network '${NETWORK_NAME}' already exists."
+#echo "Creating appman network '${NETWORK_NAME}'..."
+#sudo appman network create ${NETWORK_NAME} || echo "Network '${NETWORK_NAME}' already exists."
 #      ${NETWORK_NAME}
 
 # --- Create a OVS Bridge ---
@@ -73,14 +96,14 @@ sudo ip netns exec ${APP_NAME} ovs-vsctl add-br ${APP_NAME}
 sudo ip link set ${APP_NAME} up
 
 # --- Create a VXLAN over the Bridged Interface with IP, password, and key TAG ---
-sudo ovs-vsctl add-port ${APP_NAME} vxlanA -- set interface vxlanA type=vxlan options:remote_ip=${PSQL_IP} options:key=${PSQL_KEY} options:psk=${APP_PSK}
+sudo ovs-vsctl add-port ${APP_NAME} vxlanA -- set interface vxlanA type=vxlan options:remote_ip=${VXLAN_IP} options:key=${PSQL_KEY} options:psk=${APP_PSK}
 sudo ip link set ovs-system up
 
 # --- Run PostgreSQL container ---
 echo "Starting PostgreSQL container..."
-podman run -d \
+appman run -d \
   --name ${DB_CONTAINER_NAME} \
-  --pod=${APP_NAME} \
+  --app=${APP_NAME} \
   --net=${NETWORK_NAME} \
   -e POSTGRES_USER=${DB_USER} \
   -e POSTGRES_PASSWORD=${DB_PASSWORD} \
@@ -88,26 +111,26 @@ podman run -d \
   -v ${DB_APP}:/var/lib/pgsql/data:Z \
   postgres:alpine
 
-# Get the container's PID using podman inspect.
-CONT_PERSISTENT_PID=$(podman inspect --format '{{.State.Pid}}' "${DB_CONTAINER_NAME}")
+# Get the container's PID using appman inspect.
+CONT_PERSISTENT_PID=$(appman inspect --format '{{.State.Pid}}' "${DB_CONTAINER_NAME}")
 
 
 # Use nsenter to add a virtual Ethernet interface pair in the container's network namespace.
-sudo ip link add dev podA-ovs type veth peer name podA-netns
-sudo ip link set podA-ovs up
-sudo ip link set netns "$CONT_PERSISTENT_PID" dev podA-netns
-sudo ovs-vsctl add-port ${APP_NAME} podA-ovs
-sudo nsenter -t "$CONT_PERSISTENT_PID" -n ip link set podA-netns up
-sudo nsenter -t "$CONT_PERSISTENT_PID" -n ip addr add 10.20.30.255/24 dev podA-netns
-sudo nsenter -t "$CONT_PERSISTENT_PID" -n ip route add default via 10.20.30.1 dev podA-netns
+sudo ip link add dev ${PSQL_OVS} type veth peer name ${PSQL_NETNS}
+sudo ip link set ${PSQL_OVS} up
+sudo ip link set netns "$CONT_PERSISTENT_PID" dev ${PSQL_NETNS}
+sudo ovs-vsctl add-port ${APP_NAME} ${PSQL_OVS}
+sudo nsenter -t "$CONT_PERSISTENT_PID" -n ip link set ${PSQL_NETNS} up
+sudo nsenter -t "$CONT_PERSISTENT_PID" -n ip addr add ${PSQL_IP}/24 dev ${PSQL_NETNS}
+sudo nsenter -t "$CONT_PERSISTENT_PID" -n ip route add default via ${CT_DEF_GW} dev ${PSQL_NETNS}
 
 # --- Add a gateway to the App Brige to pass traffic SNAT/DNAT or PAT.
-sudo ip link add pod-gatewayhost type veth peer name pod-gatewayovs
-sudo ovs-vsctl add-port ${APP_NAME} pod-gatewayovs
-sudo ip link set pod-gatewayovs up
-sudo ip link set pod-gatewayhost up
-sudo ip addr add 172.16.30.1/29 dev pod-gatewayhost
-sudo nsenter -t "$CONT_PERSISTENT_PID" -n ping -c 1 10.20.30.1
+sudo ip link add app-gatewayhost type veth peer name app-gatewayovs
+sudo ovs-vsctl add-port ${APP_NAME} app-gatewayovs
+sudo ip link set app-gatewayovs up
+sudo ip link set app-gatewayhost up
+sudo ip addr add ${CT_DEF_GW}/24 dev app-gatewayhost
+sudo nsenter -t "$CONT_PERSISTENT_PID" -n ping -c 1 ${CT_DEF_GW}
 
 
 # --- Prepare Django application ---
@@ -159,14 +182,14 @@ EOF
 # Build the Django container image.
 IMAGE_NAME="django-app"
 echo "Building Django image '${IMAGE_NAME}'..."
-podman build -t ${IMAGE_NAME} .
+appman build -t ${IMAGE_NAME} .
 
 # Run the Django container.
 echo "Starting Django container..."
-podman run -d \
-  --name django \
+appman run -d \
+  --name ${DJANGO_CT_NAME} \
   --net=${NETWORK_NAME} \
-  --pod=${APP_NAME} \
+  --app=${APP_NAME} \
   -e DATABASE_NAME=${DB_NAME} \
   -e DATABASE_USER=${DB_USER} \
   -e DATABASE_PASSWORD=${DB_PASSWORD} \
@@ -174,16 +197,16 @@ podman run -d \
   ${IMAGE_NAME}
 
 cd ..
-CONT_DJANGO_PID=$(podman inspect --format '{{.State.Pid}}' "django")
+CONT_DJANGO_PID=$(appman inspect --format '{{.State.Pid}}' "${DJANGO_CT_NAME}")
 # Use nsenter to add a virtual Ethernet interface pair in the container's network namespace.
-sudo ip link add dev podB-ovs type veth peer name podB-netns
-sudo ip link set podB-ovs up
-sudo ip link set netns "$CONT_DJANGO_PID" dev podB-netns
-sudo ovs-vsctl add-port ${APP_NAME} podB-ovs
-sudo nsenter -t "$CONT_DJANGO_PID" -n ip link set podB-netns up
-sudo nsenter -t "$CONT_DJANGO_PID" -n ip addr add 10.20.30.254/24 dev podB-netns
-sudo nsenter -t "$CONT_DJANGO_PID" -n ip route add default via 10.20.30.1 dev podB-netns
-sudo nsenter -t "$CONT_DJANGO_PID" -n ping -c 1 10.20.30.1
+sudo ip link add dev ${DJANGO_OVS} type veth peer name ${DJANGO_NETNS}
+sudo ip link set ${DJANGO_OVS} up
+sudo ip link set netns "$CONT_DJANGO_PID" dev ${DJANGO_NETNS}
+sudo ovs-vsctl add-port ${APP_NAME} ${DJANGO_OVS}
+sudo nsenter -t "$CONT_DJANGO_PID" -n ip link set ${DJANGO_NETNS} up
+sudo nsenter -t "$CONT_DJANGO_PID" -n ip addr add ${DJANGO_IP}/24 dev ${DJANGO_NETNS}
+sudo nsenter -t "$CONT_DJANGO_PID" -n ip route add default via ${CT_DEF_GW} dev ${DJANGO_NETNS}
+sudo nsenter -t "$CONT_DJANGO_PID" -n ping -c 1 ${CT_DEF_GW}
 echo "Fininshed Django container installation for '${IMAGE_NAME}'..."
 
 # --- Setup Nginx container ---
@@ -207,24 +230,24 @@ server {
 EOF
 
 echo "Starting Nginx container..."
-podman run -d \
-  --name nginx \
-  --pod=${APP_NAME} \
+appman run -d \
+  --name ${NGINX_CT_NAME} \
+  --app=${APP_NAME} \
   --net=${NETWORK_NAME} \
   -v "$(pwd)/${NGINX_DIR}/default.conf":/etc/nginx/conf.d/default.conf:ro \
   nginx:alpine
 
 
-CONT_NGINX_PID=$(podman inspect --format '{{.State.Pid}}' "nginx")
+CONT_NGINX_PID=$(appman inspect --format '{{.State.Pid}}' "${NGINX_CT_NAME}")
 # Use nsenter to add a virtual Ethernet interface pair in the container's network namespace.
-sudo ip link add dev podC-ovs type veth peer name podC-netns
-sudo ip link set podC-ovs up
-sudo ip link set netns "$CONT_NGINX_PID" dev podC-netns
-sudo ovs-vsctl add-port ${APP_NAME} podC-ovs
-sudo nsenter -t "$CONT_NGINX_PID" -n ip link set podC-netns up
-sudo nsenter -t "$CONT_NGINX_PID" -n ip addr add 10.20.30.253/24 dev podC-netns
-sudo nsenter -t "$CONT_NGINX_PID" -n ip route add default via 10.20.30.1 dev podC-netns
-sudo nsenter -t "$CONT_NGINX_PID" -n ping -c 1 10.20.30.1
+sudo ip link add dev ${NGINX_OVS} type veth peer name ${NGINX_NETNS}
+sudo ip link set ${NGINX_OVS} up
+sudo ip link set netns "$CONT_NGINX_PID" dev ${NGINX_NETNS}
+sudo ovs-vsctl add-port ${APP_NAME} ${NGINX_OVS}
+sudo nsenter -t "$CONT_NGINX_PID" -n ip link set ${NGINX_NETNS} up
+sudo nsenter -t "$CONT_NGINX_PID" -n ip addr add ${NGINX_IP}/24 dev ${NGINX_NETNS}
+sudo nsenter -t "$CONT_NGINX_PID" -n ip route add default via ${CT_DEF_GW} dev ${NGINX_NETNS}
+sudo nsenter -t "$CONT_NGINX_PID" -n ping -c 1 ${CT_DEF_GW}
 
 echo "Fininshed NGINX container installation ..."
 
@@ -245,24 +268,24 @@ EOF
 
 echo "Please place your Cloudflare credentials JSON file in the '${CLOUDFLARED_DIR}' directory as 'credentials.json'."
 echo "Starting Cloudflared container..."
-podman run -d \
-  --name cloudflared \
-  --pod=${APP_NAME} \
+appman run -d \
+  --name ${CLOUDFLARED_CT_NAME} \
+  --app=${APP_NAME} \
   --net=${NETWORK_NAME} \
   -v "$(pwd)/${CLOUDFLARED_DIR}":/etc/cloudflared \
   cloudflare/cloudflared:alpine tunnel --config /etc/cloudflared/config.yml run
 
-CONT_CLOUDFLARED_PID=$(podman inspect --format '{{.State.Pid}}' "cloudflared")
+CONT_CLOUDFLARED_PID=$(appman inspect --format '{{.State.Pid}}' "${CLOUDFLARED_CT_NAME}")
 
 # Use nsenter to add a virtual Ethernet interface pair in the container's network namespace.
-sudo ip link add dev podC-ovs type veth peer name podC-netns
-sudo ip link set podC-ovs up
-sudo ip link set netns "$CONT_CLOUDFLARED_PID" dev podC-netns
-sudo ovs-vsctl add-port ${APP_NAME} podC-ovs
-sudo nsenter -t "$CONT_CLOUDFLARED_PID" -n ip link set podC-netns up
-sudo nsenter -t "$CONT_CLOUDFLARED_PID" -n ip addr add 10.20.30.253/24 dev podC-netns
-sudo nsenter -t "$CONT_CLOUDFLARED_PID" -n ip route add default via 10.20.30.1 dev podC-netns
-sudo nsenter -t "$CONT_CLOUDFLARED_PID" -n ping -c 1 10.20.30.1
+sudo ip link add dev ${CFL_OVS} type veth peer name ${CLF_NETNS}
+sudo ip link set ${CFL_OVS} up
+sudo ip link set netns "$CONT_CLOUDFLARED_PID" dev ${CFL_NETNS}
+sudo ovs-vsctl add-port ${APP_NAME} ${CFL_OVS}
+sudo nsenter -t "$CONT_CLOUDFLARED_PID" -n ip link set ${CFL_NETNS} up
+sudo nsenter -t "$CONT_CLOUDFLARED_PID" -n ip addr add ${CFL_IP}/24 dev ${CLF_NETNS}
+sudo nsenter -t "$CONT_CLOUDFLARED_PID" -n ip route add default via ${CT_DEF_GW} dev ${CFL_NETNS}
+sudo nsenter -t "$CONT_CLOUDFLARED_PID" -n ping -c 1 ${CT_DEF_GW}
 
 echo "Setup complete."
 echo "Your Django web server (with PostgreSQL) is now running behind Nginx (port 80)."
